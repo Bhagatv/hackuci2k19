6000402c2acf97e9c19da3b890c3680156ea8821
steve-ng562 <sjng1@uci.edu>
2019-02-16 01:36:49 -0800

    add

--- hackuci2k19/javascript/main.js
+++ hackuci2k19/javascript/main.js
@@ -16,8 +16,8 @@
       var player;
       function onYouTubeIframeAPIReady() {
         player = new YT.Player('player', {
-          height: '390',
-          width: '640',
+          height: '487',
+          width: '800',
           videoId: id,
           events: {
             'onReady': onPlayerReady,
@@ -28,14 +28,15 @@
 
       // 4. The API will call this function when the video player is ready.
       function onPlayerReady(event) {
-        event.target.playVideo();
+      		let time = player.getDuration();
+
       }
 
       // 5. The API calls this function when the player's state changes.
       //    The function indicates that when playing a video (state=1),
       //    the player should play for six seconds and then stop.
-      var done = false;
       function onPlayerStateChange(event) {
+      	console.log(event.data);
         if (event.data == YT.PlayerState.PLAYING && !done) {
           setTimeout(stopVideo, 6000);
           done = true;
@@ -48,7 +49,7 @@
 
 
 
-//ON SEARCH
+//ON SEARCH/////////////////////////////////
 $( "#searchClicked" ).click(function(){
 
 
@@ -57,5 +58,19 @@ $( "#searchClicked" ).click(function(){
 	console.log(url.searchParams.get("v"));
 	window.location.replace('/main.html?id=' + url.searchParams.get("v"));
 });
+///////////////////////////////////////
 
+//VIDEO PLAYER/////////////////////////
+var video = document.querySelector("#videoElement");
+var img = document.querySelector("#screenshot");
+var snapshots = [];
+if (navigator.mediaDevices.getUserMedia) {       
+    navigator.mediaDevices.getUserMedia({video: true})
+  .then(function(stream) {
+    video.srcObject = stream;
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
+}
 
