7b5df1a64129aecdd9e491d13fc5aa2ca35d360a
steve-ng562 <sjng1@uci.edu>
2019-02-16 16:06:24 -0800

    spider graph

--- hackuci2k19/images/HackUCI 2k19_files/main.js
+++ hackuci2k19/images/HackUCI 2k19_files/main.js
@@ -171,32 +171,32 @@ var myChart = new Chart(ctx, {
 	new Chart(document.getElementById("summarySpiderGraph"), {
     type: 'radar',
     data: {
-      labels: ["Anger", "Contempt", "Disgust", "Fear", "Happiness", "Neutral", "Sadness", "Surprise"],
+      labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
       datasets: [
         {
-          label: "You",
+          label: "1950",
           fill: true,
           backgroundColor: "rgba(179,181,198,0.2)",
           borderColor: "rgba(179,181,198,1)",
           pointBorderColor: "#fff",
           pointBackgroundColor: "rgba(179,181,198,1)",
-          data: personalData
+          data: [8.77,55.61,21.69,6.62,6.82]
         }, {
-          label: "HackUCI",
+          label: "2050",
           fill: true,
           backgroundColor: "rgba(255,99,132,0.2)",
           borderColor: "rgba(255,99,132,1)",
           pointBorderColor: "#fff",
           pointBackgroundColor: "rgba(255,99,132,1)",
           pointBorderColor: "#fff",
-          data: [0.1,0.1,0.2,0.2,0.1,0.1,0.1,0.1]
+          data: [25.48,54.16,7.61,8.06,4.45]
         }
       ]
     },
     options: {
       title: {
         display: true,
-        text: 'You vs. HackUCI'
+        text: 'Distribution in % of world population'
       }
     }
 });
